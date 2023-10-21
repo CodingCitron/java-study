84bffae231ef4c6498fa267c3228afc57d405dd2
sangmin <xectler@gmail.com>
2023-10-22 08:59:14 +0900

    4-6강 Web 프로그램 구현 실습

--- travel-club/travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
+++ travel-club/travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
@@ -1,13 +1,15 @@
 package io.namoosori.travelclub.web.controller;
 
+import io.namoosori.travelclub.web.aggregate.club.TravelClub;
 import io.namoosori.travelclub.web.service.ClubService;
 import io.namoosori.travelclub.web.service.sdo.TravelClubCdo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import io.namoosori.travelclub.web.shared.NameValueList;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/club")
 public class ClubController {
 
     private ClubService clubService;
@@ -16,10 +18,33 @@ public class ClubController {
         this.clubService = clubService; /*  */
     }
 
-    @PostMapping("/club")
+    @PostMapping
     public String register(@RequestBody TravelClubCdo travelClubCdo) {
         return clubService.registerClub(travelClubCdo);
     }
 
+    @GetMapping("/all")
+    public List<TravelClub> findAll() {
+        return clubService.findAll();
+    }
 
+    @GetMapping("/{clubId}")
+    public TravelClub find(@PathVariable String clubId) {
+        return  clubService.findClubById(clubId);
+    }
+
+    @GetMapping // localhost:8090/club?name="JavaClub"
+    public List<TravelClub> findByName(@RequestParam String name) {
+        return clubService.findClubsByName(name);
+    }
+
+    @PutMapping("/{clubId}")
+    public void modify(@PathVariable String clubId, @RequestBody NameValueList nameValueList) {
+        clubService.modify(clubId, nameValueList);
+    }
+
+    @DeleteMapping("/{clubId}")
+    public void delete(@PathVariable String clubId) {
+        clubService.remove(clubId);
+    }
 }
