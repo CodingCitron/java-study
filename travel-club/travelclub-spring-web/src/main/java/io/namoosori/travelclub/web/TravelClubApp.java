ca7be3aaae42fa153b5fdbcc2b7fc0dd5537c45e
sangmin <xectler@gmail.com>
2023-10-24 05:33:11 +0900

    Spring Data JPA 실습

--- travel-club/travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/TravelClubApp.java
+++ travel-club/travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/TravelClubApp.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravelClubApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(TravelClubApp.class);
+        SpringApplication.run(TravelClubApp.class, args);
     }
 }
